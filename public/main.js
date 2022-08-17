326b9175e8a550d8bcb0eb4bc565f6e085feb0d5
Alexandra Printzios <alex.printzios@gmail.com>
2022-08-16 22:20:29 -0600

    ready pseudocode

--- public/main.js
+++ public/main.js
@@ -3,24 +3,42 @@ function onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
   }
 
-setTimeout(()=> {
-    let days = document.getElementsByClassName('react-calendar__month-view__days__day')
-    days = [...days];
-    days.forEach(elem => elem.style.color = "black")
-    let sessionEl = document.getElementById('sessions')
-    //get unique days from sessions
-    let sessionDays = JSON.parse(sessionEl.value);
-    sessionDays = sessionDays.map(elem => elem.date)
-    let availableDays = sessionDays.filter(onlyUnique);
-    availableDays = availableDays.map(elem => (new Date(elem)).toString().substring(0,15));
-    days.forEach(day => {
-        // console.log(new Date([...day.children][0].ariaLabel))
-        // new Date([...day.children][0].ariaLabel).toString().substring(0,15),availableDays[0])
-        // console.log(availableDays.includes(new Date([...day.children][0].ariaLabel)))
-        if (availableDays.includes(new Date([...day.children][0].ariaLabel).toString().substring(0,15))) {
-            day.style.color = 'magenta';
-            day.style.fontWeight = 'bold';
-        }
-    })
-}, 800);
-// console.log(document.getElementById("root"))
+setTimeout(() => {
+    let navArrows = document.getElementsByClassName('react-calendar__navigation__arrow');
+    navArrows = [...navArrows]
+    console.log(navArrows)
+    navArrows.forEach(arrow => arrow.addEventListener('click', () => styleCalendar()))
+}, 100)
+
+
+function styleCalendar(){
+    setTimeout(()=> {
+        let days = document.getElementsByClassName('react-calendar__month-view__days__day')
+        days = [...days];
+        days.forEach(elem => {
+            elem.style.color = "black"
+        })
+        let sessionEl = document.getElementById('sessions')
+        //get unique days from sessions
+        let sessionDays = JSON.parse(sessionEl.value);
+        sessionDays = sessionDays.map(elem => elem.date)
+        let availableDays = sessionDays.filter(onlyUnique);
+        availableDays = availableDays.map(elem => (new Date(elem)).toString().substring(0,15));
+        days.forEach(day => {
+            // console.log(new Date([...day.children][0].ariaLabel))
+            // new Date([...day.children][0].ariaLabel).toString().substring(0,15),availableDays[0])
+            // console.log(availableDays.includes(new Date([...day.children][0].ariaLabel)))
+            if (availableDays.includes(new Date([...day.children][0].ariaLabel).toString().substring(0,15))) {
+                day.style.color = 'magenta';
+                day.style.fontWeight = 'bold';
+            }
+        })
+    }, 250);
+}
+
+styleCalendar()
+
+//change all bgs to white
+//get selected date element and change element background to light pink
+//get selected date element and retrieve date then find all items in the sessions array that match that date
+//display all times from the matched sessions in the sessiontimes div
